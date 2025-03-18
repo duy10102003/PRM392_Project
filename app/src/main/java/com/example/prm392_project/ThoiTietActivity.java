07cf597936cb309d42c1798a4dce468fbf9ef69a
Thai Nguyen Hoang <thai03040302@gmail.com>
2025-03-18 23:33:39 +0700

    api thoi tiet

--- app/src/main/java/com/example/prm392_project/ThoiTietActivity.java
+++ app/src/main/java/com/example/prm392_project/ThoiTietActivity.java
@@ -47,13 +47,13 @@ public class ThoiTietActivity extends AppCompatActivity {
         txtMay = findViewById(R.id.textviewMay);
         txtNgayCapNhat = findViewById(R.id.textviewNgayCapNhat);
         imgIcon = findViewById(R.id.imgIcon);
-        GetCurrentWeatherData("Hanoi");
+        GetCurrentWeatherData("Saigon");
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String city = edtSearch.getText().toString();
                 if (city.equals("")){
-                    City = "Hanoi";
+                    City = "Saigon";
                     GetCurrentWeatherData(City);
                 }
                 else {
@@ -67,7 +67,68 @@ public class ThoiTietActivity extends AppCompatActivity {
 
     }
     public void GetCurrentWeatherData(String data){
+        RequestQueue requestQueue= Volley.newRequestQueue(ThoiTietActivity.this);
+        String url = "https://api.openweathermap.org/data/2.5/weather?q="+data+"&appid=037d21cb14640ecd9a4f4eca49e7ff3f";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String day = jsonObject.getString("dt");
+                            String name = jsonObject.getString("name");
+                            txtThanhpho.setText(name);
 
+                            long l = Long.valueOf(day);
+                            Date date = new Date(l*1000L);
+                            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE yyyy-MM-dd \n           HH-mm-ss");
+                            String Day  = simpleDateFormat.format(date);
+
+                            txtNgayCapNhat.setText(Day);
+                            JSONArray jsonArrayWeather = jsonObject.getJSONArray("weather");
+                            JSONObject jsonObjectWeather = jsonArrayWeather.getJSONObject(0);
+
+                            String TrangThai = jsonObjectWeather.getString("main");
+                            String icon = jsonObjectWeather.getString("icon");
+
+                            Picasso picasso = Picasso.get();
+                            picasso.load("https://openweathermap.org/img/wn/" + icon + ".png").into(imgIcon);
+                            txtTrangThai.setText(TrangThai);
+
+                            JSONObject jsonObjectMain = jsonObject.getJSONObject("main");
+                            String nhietdo= jsonObjectMain.getString("temp");
+                            String doam = jsonObjectMain.getString("humidity");
+
+                            Double a = Double.valueOf(nhietdo);
+                            String Nhietdo = String.valueOf(a.intValue() - 273);
+
+                            txtNhietDo.setText(Nhietdo+"°C");
+                            txtDoam.setText(doam+"%");
+
+                            JSONObject jsonObjectWind = jsonObject.getJSONObject("wind");
+                            String gio = jsonObjectWind.getString("speed");
+                            txtGio.setText(gio+"m/s");
+
+                            JSONObject jsonObjectClouds = jsonObject.getJSONObject("clouds");
+                            String may = jsonObjectClouds.getString("all");
+                            txtMay.setText(may+"%");
+
+
+                            JSONObject jsonObjectSys = jsonObject.getJSONObject("sys");
+                            String country = jsonObjectSys.getString("country");
+                            txtQuocGia.setText("Tên quốc gia : " + country);
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        requestQueue.add(stringRequest);
     }
 
 }
